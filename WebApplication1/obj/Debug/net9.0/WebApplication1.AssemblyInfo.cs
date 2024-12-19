8858f2be67855435d6f6e5315d86d6cfd22405eb
claud <claudiupetrila@gmail.com>
2024-12-19 23:17:50 +0200

    Minor spelling mistake

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a66eae7c801aa623ec353cd10c99a81b51fc265")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2977ec4f8c1f3a969694292cf02093148e15db02")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
